4984fcae1311b7d9012c99c5c5aeaf4e5c706abc
FWanning <frankwanning@web.de>
2015-05-13 19:30:50 +0200

    Update HAL_Interpreter.java
    kleinigkeiten

--- src/hal_interpreter/HAL_Interpreter.java
+++ src/hal_interpreter/HAL_Interpreter.java
@@ -10,13 +10,57 @@ public class HAL_Interpreter {
     
     
     
-    public static void main(String[] args) throws IOException {
+   public static void main(String[] args) throws IOException {
        
         File input = new File("test1.txt");  // Test Datein = test.txt,test1.txt
-        ProgramMemory test = new ProgramMemory(input);
-        System.out.println(test.getInstruktion(12));
+        Register testReg= new Register();
+        ProgramMemory testPM = new ProgramMemory(input);
+        Acc testAcc = new Acc();
+        
+  
+        for(int i =0;i<testPM.getProgramSize();i++){
+        
+        
+            
+            
+            
+        
+        }
+       
+        
         
     }
+    
+    
+     public void instructionExecute(String instruction,int isntructionConstant){
+       
+        switch(instruction){
+
+             case "START":
+            
+            
+            
+             break;
+             case "STOP":break;
+             case "OUT":break; 
+             case "IN":break; 
+             case "LOAD":break; 
+             case "LOADNUM":break; 
+             case "STORE":break; 
+             case "JUMPNEG":break; 
+             case "JUMPPOS":break;       
+             case "JUMPNULL":break; 
+             case "JUMP":break; 
+             case "ADD":break; 
+             case "ADDNUM":break; 
+             case "SUB":break; 
+             case "MUL":break;
+             case "DIV":break; 
+             case "SUBNUM":break; 
+             case "MULNUM":break; 
+             case "DIVNUM":break; 
+             default:break; 
+   }
     
 }
 
